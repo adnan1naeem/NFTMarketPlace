65137e9775f0da18a64eb6e2abd95ca991f40c7e
mohsinghafoor <ch.mohsin.ghafoor@gmail.com>
2022-06-16 14:25:38 +0500

    setup folder structure

---
+++ src/assets/Data.js
@@ -0,0 +1,23 @@
+export const blogsData = [
+  {
+    heading: "Mindfulness Activities for Kids & Toddlers with NFT",
+    text: "Poverty of in the better in little. Did out held be entered it small...",
+    date: "09 Feb 2022",
+    numCmt: "30",
+    id: "min",
+  },
+  {
+    heading: "Save Thousands Of Lives Through This NFT",
+    text: "Staple was it myself. The both writing and area century...",
+    date: "11 Mar 2022",
+    numCmt: "87",
+    id: "sav",
+  },
+  {
+    heading: "Honoring Black History Month with Toddlers",
+    text: "At but physics sofa a explorations are trusted curiously ...",
+    date: "31 June 2022",
+    numCmt: "60",
+    id: "hon",
+  },
+];
