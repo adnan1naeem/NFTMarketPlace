61a786d3451400b66d8474d00c9dde7d9c320878
mohsinghafoor <ch.mohsin.ghafoor@gmail.com>
2022-06-16 14:25:53 +0500

    setup folder structure

---
+++ src/Pages/Home/Blogs/index.jsx
@@ -0,0 +1,6 @@
+import { blogsData } from "assets/Data";
+import React from "react";
+
+export const Blogs = () => {
+  return <div></div>;
+};
---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Blogs } from "./Blogs";
+
+export const Home = () => {
+  return (
+    <div>
+      <Blogs />
+    </div>
+  );
+};
