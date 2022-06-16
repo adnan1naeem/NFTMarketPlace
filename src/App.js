ed43a4ebef831526a4d251307714658feaa8268b
mohsinghafoor <ch.mohsin.ghafoor@gmail.com>
2022-06-16 12:54:48 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-red-500 bg-gray-300">Tailwind Setup</h1>
+    </div>
+  );
+}
+
+export default App;
