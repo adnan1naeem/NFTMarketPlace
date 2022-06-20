7a001bf61facf616d78de936829a983465681c82
mohsinghafoor <ch.mohsin.ghafoor@gmail.com>
2022-06-20 10:50:45 +0500

    Made footer

--- src/Pages/Footer/index.jsx
+++ src/Pages/Footer/index.jsx
@@ -1,25 +1,28 @@
-import { Box } from "@mui/material";
+import { Box, styled } from "@mui/material";
 import { communityData, exploreData, navigationData } from "assets/Data";
 import React from "react";
 import { Shortcuts } from "./Shortcuts";
 import { SocialMedia } from "./SocialMedia";
 import { Subscribe } from "./Subscribe";
-
+import footerbg from "../../assets/Images/footer-bg.svg";
+const Root = styled(Box)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "space-between",
+  margin: "3% 7%",
+  flexWrap: "wrap",
+  backgroundImage: "url`(${footerbg})`",
+  [theme.breakpoints.only("xs")]: {
+    margin: "3%",
+  },
+}));
 export const Footer = () => {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "space-between",
-        margin: "3% 7%",
-        flexWrap: "wrap",
-      }}
-    >
+    <Root>
       <SocialMedia />
       <Shortcuts data={navigationData} heading={"Navigation"} />
       <Shortcuts data={exploreData} heading={"Explore"} />
       <Shortcuts data={communityData} heading={"Community"} />
       <Subscribe />
-    </Box>
+    </Root>
   );
 };
